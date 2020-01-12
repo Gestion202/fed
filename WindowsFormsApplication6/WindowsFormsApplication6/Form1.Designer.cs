4911b78b1616b71d058d1680f43abd219747b13e
fedu1994 <ferdaoussakhchoun@gmail.com>
2020-01-12 19:00:09 +0100

    new branche ferdaouss

--- WindowsFormsApplication6/WindowsFormsApplication6/Form1.Designer.cs
+++ WindowsFormsApplication6/WindowsFormsApplication6/Form1.Designer.cs
@@ -46,9 +46,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(131, 160);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.Size = new System.Drawing.Size(95, 13);
             this.label1.TabIndex = 1;
-            this.label1.Text = "hello world";
+            this.label1.Text = "branche ferdaouss";
             // 
             // Form1
             // 
